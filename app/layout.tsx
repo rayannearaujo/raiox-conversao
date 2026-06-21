9af47d1d1f6e8c7953428df79a1a6a957df6b56c
Rayanne Araújo <kaelinearaujo@gmail.com>
2026-06-21 17:17:05 -0300

    Add Google Tag Manager scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,20 +15,43 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="h-full antialiased">
-  <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col">
 
-    <Script id="clarity" strategy="afterInteractive">
-      {`
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "x9upc9ioii");
-      `}
-    </Script>
+        {/* ===== GOOGLE TAG MANAGER (BODY) ===== */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MJ2Q365N"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
-    {children}
-  </body>
-</html>
+        {/* ===== GOOGLE TAG MANAGER (SCRIPT) ===== */}
+        <Script id="gtm" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer' ? '&l='+l : '';
+
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MJ2Q365N');
+          `}
+        </Script>
+        
+        {children}
+
+      </body>
+    </html>
   );
 }
